a05b2641f63c84f0f3885d6961e7cc48f0d45c14
dew.maple <dew.maple@gmail.com>
2018-03-08 00:16:59 +0800

    add text on recognized number image

--- number_rec/knn.py
+++ number_rec/knn.py
@@ -20,28 +20,33 @@ for cnt in contours:
 
 training = np.array(train_cells).reshape(-1, 900).astype(np.float32)
 
-k = np.arange(10)
-train_labels = k[:, np.newaxis]
+# k = np.arange(10)
+train_labels = np.arange(10)
 test_labels = train_labels.copy()
 
 knn = cv2.ml.KNearest_create()
 knn.train(training, cv2.ml.ROW_SAMPLE, train_labels)
 
 test_image = cv2.imread('./data/numbers.jpg')
-cols, rows = test_image.shape[:2]
 
 test_gray = cv2.cvtColor(test_image, cv2.COLOR_BGR2GRAY)
 _, threshold = cv2.threshold(test_gray, 100, 255, cv2.THRESH_BINARY_INV)
 im2, contours, hierarchy = cv2.findContours(threshold, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
 contours = sorted(contours, key=lambda c: (cv2.boundingRect(c)[1], cv2.boundingRect(c)[0]))
 test_cells = []
+pnts = []
 for (i, cnt) in enumerate(contours):
     x, y, w, h = cv2.boundingRect(cnt)
     test_cells.append(cv2.resize(test_gray[y:y + h, x:x + w], (30, 30)))
+    pnts.append((x, y))
 
 testing = np.array(test_cells).reshape(-1, 900).astype(np.float32)
 
 ret, result, neighbours, dist = knn.findNearest(testing, k=1)
+for (i, p) in enumerate(pnts):
+    cv2.putText(test_image, '{}'.format(int(result[i])), (p[0], p[1]), cv2.FONT_HERSHEY_SIMPLEX, 0.65, (0, 255, 0), 1)
+cv2.imshow('result', test_image)
+
 print('ret:{}'.format(ret))
 print('result:{}'.format(result))
 print('neighbours:{}'.format(neighbours))
@@ -53,3 +58,6 @@ print('dist:{}'.format(dist))
 # correct = np.count_nonzero(matches)
 # accuracy = correct * 100.0 / result.size
 # print(accuracy)
+
+cv2.waitKey(0)
+cv2.destroyAllWindows()
